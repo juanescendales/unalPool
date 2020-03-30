e6a0a76229f3c5f4c2ae83a726f5567444e119a8
juanca548 <47082957+juanca548@users.noreply.github.com>
2020-03-29 22:31:14 -0500

    Avances

---
+++ app/src/main/java/com/example/unalpool/Models/Vehicle.kt
@@ -0,0 +1,5 @@
+package com.example.unalpool.Models
+
+class Vehicle (val matricula:String, val soatUrl:String, val tecnomecanicaUrl:String, val rutUrl:String, val fotoDelVehiculo:String) {
+    constructor() : this("null", "null", "null", "null","null")
+}
